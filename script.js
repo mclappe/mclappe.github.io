cce5a823b736eb54956e1c1c1bc6e16b6460a8b9
mclappe <158617974+mclappe@users.noreply.github.com>
2024-02-02 15:59:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "LET's GOOOOO, see you on the 14th bb😘😘";
+    yesMessage.textContent = "LET'S GOOOOO, see you on the 14th bb😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
